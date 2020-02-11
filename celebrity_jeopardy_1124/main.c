0a755c45590f39ed62ed9e7f9adf7f463ce13bff
bibo7086 <bibo7086@gmail.com>
2020-02-11 17:06:22 +0300

    Initial Commit

---
+++ celebrity_jeopardy_1124/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  celebrity_jeopardy_1124
+//
+//  Created by Saeed Ali on 3/27/17.
+//  Copyright © 2017 Saeed Ali. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h> 
+
+
+int main()
+{
+    char a[100];
+    
+    while (gets(a)) {
+        
+        printf("%s\n", a);
+    
+    }
+    
+    return 0 ;
+}
